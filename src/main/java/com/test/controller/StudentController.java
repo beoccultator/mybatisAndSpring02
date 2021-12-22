58838d697e931a60976adb85dccc5ee78e7cf8b7
liuzong <liuzong@163.com>
2021-12-22 16:33:36 +0800

    first commit of main

--- src/main/java/com/test/controller/StudentController.java
+++ src/main/java/com/test/controller/StudentController.java
@@ -1,4 +1,11 @@
 package com.test.controller;
 
+import com.test.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
 }
